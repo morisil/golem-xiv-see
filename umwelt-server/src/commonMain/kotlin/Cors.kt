45586f694d7e18c977107de339a7783d35a64d2d
Kazik Pogoda <morisil@xemantic.com>
2026-06-28 13:59:05 +0200

    Add Cors.kt

---
+++ umwelt-server/src/commonMain/kotlin/Cors.kt
@@ -0,0 +1,39 @@
+/*
+ * Umwelt - The web as your AI agent's Umwelt - every page transduced into the language a model natively perceives
+ * Copyright (C) 2026  Kazimierz Pogoda / Xemantic
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.xemantic.umwelt.server
+
+import io.ktor.http.HttpHeaders
+import io.ktor.http.HttpMethod
+import io.ktor.server.application.Application
+import io.ktor.server.application.install
+import io.ktor.server.plugins.cors.routing.CORS
+
+/**
+ * Allows the separately-served browser app (a different origin) to call this
+ * API from `fetch`. `anyHost()` keeps local development friction-free.
+ */
+fun Application.corsModule() {
+    install(CORS) {
+        anyHost()
+        allowHeader(HttpHeaders.ContentType)
+        allowMethod(HttpMethod.Get)
+        allowMethod(HttpMethod.Post)
+        allowMethod(HttpMethod.Delete)
+    }
+}
