6680ce457d1c4a97889a83e7ae4ea5eb5158b8d1
Kazik Pogoda <morisil@xemantic.com>
2026-06-28 15:24:56 +0200

    add UmweltTestSupport.kt

---
+++ umwelt-server/src/commonTest/kotlin/UmweltTestSupport.kt
@@ -0,0 +1,34 @@
+/*
+ * Umwelt - The web as your AI agent's Umwelt - every page transduced into the language a model natively perceives
+ * Copyright (C) 2026  Kazimierz Pogoda / Xemantic
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.xemantic.umwelt.server
+
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.serialization.kotlinx.json.json
+import io.ktor.server.testing.ApplicationTestBuilder
+
+fun ApplicationTestBuilder.umweltTestApp() {
+    application {
+        umweltAppModule()
+    }
+    client = createClient {
+        install(ContentNegotiation) {
+            json()
+        }
+    }
+}
