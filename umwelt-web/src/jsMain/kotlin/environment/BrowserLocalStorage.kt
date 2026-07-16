0a0ee6785ff00290a847b68bc1bd9b8733cf2058
Kazik Pogoda <morisil@xemantic.com>
2026-07-16 23:06:30 +0200

    add BrowserLocalStorage.kt

---
+++ umwelt-web/src/jsMain/kotlin/environment/BrowserLocalStorage.kt
@@ -0,0 +1,36 @@
+/*
+ * Golem XIV - Autonomous metacognitive AI system with semantic memory and self-directed research
+ * Copyright (C) 2026  Kazimierz Pogoda / Xemantic
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.xemantic.umwelt.web.environment
+
+import com.xemantic.umwelt.viewmodel.environment.LocalStorage
+import kotlinx.browser.localStorage
+import org.w3c.dom.get
+import org.w3c.dom.set
+
+class BrowserLocalStorage : LocalStorage {
+
+    override fun get(
+        key: String
+    ): String? = localStorage[key]
+
+    override fun set(key: String, value: String) {
+        localStorage[key] = value
+    }
+
+}
